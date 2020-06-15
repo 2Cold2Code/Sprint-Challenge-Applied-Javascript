ce0fa044b9a498228f12317de2600a2a726e9bed
Adam Osburn <wizardofosburn@gmail.com>
2020-06-14 21:02:43 -0400

    moved functions into file, made GET request (axios), made function for tabs (not operable)

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,46 @@
 //
 //  Each tab should look like this:
 //    <div class="tab">topic here</div>
+
+const noel = (selector) => document.createElement(selector);
+
+const gretel = (selector)=> document.querySelector(selector);
+
+const gretels = (selector) => document.querySelectorAll(selector);
+
+const append = (appendTo, element) => {
+    let parent = appendTo;
+    parent.appendChild(element);
+    return parent;
+}
+
+axios.get(
+    'https://lambda-times-backend.herokuapp.com/topics'
+    )
+    .then(
+        res => {
+            console.log(res)
+            append('.title', Tabs(res))
+         }
+     )
+     .catch(
+         err => {
+             console.log(err)
+         }
+     )
+
+function Tabs(resData){
+    const {data} = resData;
+    const {topics} = data;
+    const tab = noel('div');
+    topics.forEach(
+        topic => {
+            let element = noel('p');
+            element.textContent = topic;
+            append(tab, element);
+            return tab;
+        }
+        )
+        return tab
+}
+
