ec532f19c5019c0c13b15f96498ed3239cd33c7e
Adam Osburn <wizardofosburn@gmail.com>
2020-06-14 20:16:08 -0400

    made functions to create, select and append elements made function component that makes the elements, made function component that appends to html

--- components/Header/index.js
+++ components/Header/index.js
@@ -11,4 +11,45 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+export const noel = (selector) => document.createElement(selector);
+
+export const gretel = (selector)=> document.querySelector(selector);
+
+export const gretels = (selector) => document.querySelectorAll(selector);
+
+export const append = (appendTo, element) => {
+    let parent = appendTo;
+    let child = element;
+    parent.appendChild(element);
+    return parent;
+}
+
+const header = gretel('.header-container');
+
+function Header(){
+    const headC = append(header, makeHeader())
+
+    return headC;
+}
+
+Header();
+
+function makeHeader(){
+
+    const header = noel('header');
+    header.classList.add('header');
+
+    const date = noel('span');
+    date.classList.add('date');
+    date.textContent = 'SMARCH 28, 2019';
+    append(header, date);
+
+    const title = noel('h1');
+    title.textContent = 'Lambda Times';
+    append(header, title);
+
+    const temp = noel('span');
+    temp.textContent = '98\u{00B0}';
+    append(header, temp)
+    return header
+}
